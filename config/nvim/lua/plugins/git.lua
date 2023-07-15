4715b5b78b15cd7672dd11e9f1963f0b985f3e59
rurumimic <10915235+rurumimic@users.noreply.github.com>
2023-07-16 00:18:03 +0900

    neogit.lua

---
+++ config/nvim/lua/plugins/git.lua
@@ -0,0 +1,13 @@
+return {
+  { 
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("neogit").setup({})
+    end,
+  }
+}
+
